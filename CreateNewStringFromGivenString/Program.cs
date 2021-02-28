0f8fe7d301f11a45f4d14481466e327675548318
BBranko93 <bane_b80@outlook.com>
2021-02-27 20:37:22 -0700

    Add project files.

---
+++ CreateNewStringFromGivenString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CreateNewStringFromGivenString
+{
+    class Program
+    {
+        //Write a C# program to create a new string from a given string
+        //(length 1 or more ) with the first character added at the front and back.
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sentence : ");
+            string words = Console.ReadLine();
+
+            NewSentence(words);
+
+        }
+
+        static string NewSentence(string words)
+        {
+            if (words.Length > 1)
+            {
+                string s = words.Substring(0, 1);
+                Console.WriteLine(s + words + s);
+
+            }
+
+            return (words);
+        }
+    }
+}
